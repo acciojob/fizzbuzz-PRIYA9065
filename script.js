f33cdaf92a96c23633cfcdc226f78d60f2ae3b5f
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-07-03 16:44:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const arr = [];
+function hello(){
+	for(let i = 1; i <= 100; i++){
+		if(i % 3 == 0){
+			arr.push("Fizz");
+		}
+		else if(i % 5 == 0){
+			arr.push("Buzz");
+		}
+		else if(i % 3 == 0 && i % 5 == 0){
+			arr.push("FizzBuzz");
+		}
+
+		else{
+			arr.push(i.toString());
+		}
+	alert(arr);
+	}
+}
